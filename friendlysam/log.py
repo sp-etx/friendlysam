18cab1ab09a2ac92ee824c8a09c52ce0bbffa707
Rasmus Einarsson <rasmus.einarsson@sp.se>
2014-12-19 15:03:55 +0100

    Add logging

---
+++ friendlysam/log.py
@@ -0,0 +1,10 @@
+import logging
+
+root_logger = logging.getLogger()
+
+stderr_handler = logging.StreamHandler()
+stderr_handler.setFormatter(logging.Formatter('[%(levelname)8s] (%(name)s) %(message)s'))
+root_logger.addHandler(stderr_handler)
+
+def get_logger(name):
+    return root_logger.getChild(name)
