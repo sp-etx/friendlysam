2c79b4523c481702eaf371ce2aea3d7c1bd49871
Rasmus Einarsson <rasmus.einarsson@sp.se>
2014-12-20 12:55:46 +0100

    Rewrite constraints a bit
    All constraints now inherit Constraint.
    Problem now has replacement rules and can simplify
    its constraints prior to solve() call.
    Constraints are now added with Problem.add_constraint()
    instead of just modifying the set `constraints`. The
    set `constraints` is now a property.

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from friendlysam.parts.process import Process
 import sympy
 import friendlysam.optimization as opt
-from friendlysam.optimization.solvers.gurobi import GurobiSolver
+#from friendlysam.optimization.solvers.gurobi import GurobiSolver
 
 class ProcessA(Process):
     """docstring for ProcessA"""
@@ -12,7 +12,7 @@ class ProcessA(Process):
         # x, y, z = [self.variable(str(i)) for i in range(3)]
         # self.constrain(opt.make_constraints((x,y,z), sos1=True, lb=0, ub=3))
 
-        v = self.variable_collection('arne', ub=3)
+        v = self.variable_collection('x', ub=3)
         self.v = v
         self.production[1] = lambda t: v[t] * a
         self.constrain(lambda t: v[t] * 1.1 <= v[t-1])
@@ -27,13 +27,16 @@ x = ProcessA(20)
 
 p = opt.Problem()
 
-times = range(40)
-p.constraints = x.constraints(times)
+times = range(5)
+p.rules[x.v[0]] = 1
+p.rules[x.v[-1]] = 1
+p.add_constraints(x.constraints(times))
 p.sense = opt.Sense.maximize
 
+p.simplify()
 
-p.objective = sum([x.production[1](t) for t in times])
-p.solver = GurobiSolver()
-p.solve()
-for t in times:
-    print(p._solution[x.v[t]])
+# p.objective = sum([x.production[1](t) for t in times])
+# p.solver = GurobiSolver()
+# p.solve()
+# for t in times:
+#     print(p._solution[x.v[t]])
