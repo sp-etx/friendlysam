ffdee55f2f13d2efd52e47a84ee9a3b89aac6251
Rasmus Einarsson <rasmus.einarsson@gmail.com>
2015-03-26 20:51:32 +0100

    Pasteurize tests

--- tests/test_indexing.py
+++ tests/test_indexing.py
@@ -1,3 +1,11 @@
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from builtins import super
+from builtins import range
+from future import standard_library
+standard_library.install_aliases()
 from itertools import chain
 from nose.tools import raises
 from friendlysam.model import Node, Storage, Cluster, ResourceNetwork
