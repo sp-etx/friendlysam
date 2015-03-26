d81007bb661fc8af5f01ea633e88d22d6013b808
Rasmus Einarsson <rasmus.einarsson@sp.se>
2015-03-26 10:47:28 +0100

    Make Python2.7 compatible
    Using package future which provides the tool pasteurize
    
    pasteurized all python files
    
    added enum34 and future to requirements

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+from __future__ import absolute_import
+from builtins import range
+from builtins import super
+from future import standard_library
+standard_library.install_aliases()
 import itertools
 from friendlysam.model import Node, Storage, Cluster, ResourceNetwork
 from friendlysam.optimization import *
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,3 +1,11 @@
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+from builtins import range
+from builtins import super
+from future import standard_library
+standard_library.install_aliases()
 from friendlysam.model import Node, Storage, Cluster, ResourceNetwork
 from friendlysam.optimization import *
 from friendlysam.optimization.pyomoengine import PyomoProblem
