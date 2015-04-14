c6b6221637584e41642d5168f57507c52cf36e1f
Rasmus Einarsson <rasmus.einarsson@sp.se>
2015-04-14 14:32:41 +0200

    Improve setup script
    Now it's enough to just
    
    python setup.py develop

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
 
+import os
 from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+
 setup(
-name='friendlysam',
-version='',
-packages=['friendlysam'],
-url='',
-license='',
-author='',
-author_email='',
-description=''
-)
+    name='friendlysam',
+    version='',
+    packages=['friendlysam'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description='',
+    install_requires=required
+    )
