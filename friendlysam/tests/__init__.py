427423c01aa9e4e0763a73d9603bc78b883a546e
Rasmus Einarsson <rasmus.einarsson@sp.se>
2015-04-14 14:32:01 +0200

    Remove last (?) trace of pasteurize

--- friendlysam/tests/__init__.py
+++ friendlysam/tests/__init__.py
@@ -1,14 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import unicode_literals
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
-from builtins import range
-from builtins import super
-from future import standard_library
-standard_library.install_aliases()
-
 from friendlysam.optimization.pulpengine import PulpSolver
 
 default_solver = PulpSolver()
