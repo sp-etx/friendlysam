55f3c97729ff766d1d95610e8258cfe26aced0a3
Rasmus Einarsson <rasmus.einarsson@sp.se>
2015-05-28 14:29:12 +0200

    __div__ --> __truediv__ (Python 3, stupid)

--- friendlysam/optimization.py
+++ friendlysam/optimization.py
@@ -146,7 +146,7 @@ class _MathEnabled(object):
     def __rmul__(self, other):
         return 0 if _is_zero(other) else Mul(other, self)
 
-    def __div__(self, other):
+    def __truediv__(self, other):
         return self * (1/other) # Takes care of division by scalars at least
 
     def __neg__(self):
